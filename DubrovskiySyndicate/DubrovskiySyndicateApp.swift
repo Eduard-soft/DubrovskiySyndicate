d21d5ddc00dc69d50a2b9cfae209df4d9e8acc28
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2022-04-03 15:47:00 +0300

    Initial Commit

---
+++ DubrovskiySyndicate/DubrovskiySyndicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubrovskiySyndicateApp.swift
+//  DubrovskiySyndicate
+//
+//  Created by Эдуард on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DubrovskiySyndicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
